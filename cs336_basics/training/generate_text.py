068f5d6f188b115ce59b3be0aa38e0028ad74298
Kai Follmann <kai.follmann@tum.de>
2026-04-24 12:04:22 +0200

    added generation parts

--- cs336_basics/training/generate_text.py
+++ cs336_basics/training/generate_text.py
@@ -1,6 +1,10 @@
 import torch
 
 from cs336_basics.model.transformer_lm import TransformerLanguageModel
+from cs336_basics.tokenizer.encoder import Tokenizer
+
+import os
+
 
 
 from cs336_basics.training.checkpointing import load_checkpoint
@@ -24,13 +28,29 @@ def generate():
     num_heads = 16
     num_layers = 4
     
+    special_tokens = ['<|endoftext|>']
+
+    os.makedirs("checkpoints", exist_ok=True)
+    ckpt_path = os.path.join("checkpoints", "latest.pt")
+
+    vocab_path = "vocab/vocab_full.json"
+    merges_path = "merges/merges_full.json"
+    
     language_model = TransformerLanguageModel(
         vocab_size=vocab_size, context_length=context_length,
         num_layers=num_layers, d_model=d_model, d_ff=d_ff,
         num_heads=num_heads, rope_theta=rope_theta, device=device,
     )
+    
+    load_checkpoint("checkpoints/latest.pt", model=language_model, optimizer=None)
+    
     language_model = torch.compile(language_model, backend="aot_eager")
     
+    tokenizer = Tokenizer.from_files(vocab_path, merges_path, special_tokens)
+    
+    start_tokens = tokenizer.encode("<|endoftext|>")
+    
+    x = torch.tensor([start_tokens], dtype=torch.long, device=device) # shape (1,1)
     
 
     
